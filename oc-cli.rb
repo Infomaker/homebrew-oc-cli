b8162a33e7430f926adc79c4c78dda682c5c771e
Hans Bringert <hans.bringert@infomaker.se>
2018-08-07 11:47:34 +0200

    test

--- oc-cli.rb
+++ oc-cli.rb
@@ -2,21 +2,23 @@ class OcCli < Formula
   desc "Open Content CLI"
   homepage "https://bitbucket.org/infomaker/oc-cli"
   url "https://s3-eu-west-1.amazonaws.com/open-content-artifacts/oc-cli/oc-cli.tar.gz"
-  sha256 "cff9f84faba51cfccbfd07831664c7972cfc243041415be9d27ac287ac4f22fd"
+  sha256 "9092925b72db9915a451257c0d759d343181e50441ef54f6a6ae1cb7c5071b94"
 
   depends_on "go" => :build
 
   def install
 	  ENV["GOPATH"] = buildpath
 
-    system "go", "get", "github.com/aws/aws-sdk-go"
-    system "go", "get", "-u", "github.com/spf13/cobra/cobra"
-    system "go", "get", "github.com/cpuguy83/go-md2man"
+    system "bin/fix.sh"
+
+    # system "go", "get", "github.com/aws/aws-sdk-go"
+    # system "go", "get", "-u", "github.com/spf13/cobra/cobra"
+    # system "go", "get", "github.com/cpuguy83/go-md2man"
     
     # Generate the bash completion script 
-    system "go", "run", "oc-cli-bash-completion.go"
+    # system "go", "run", "oc-cli-bash-completion.go"
 
- 	  system "go", "build", "-o", "oc-cli", "-ldflags", "-X main.appVersion=0.2", "oc-cli.go"
+ 	  # system "go", "build", "-o", "oc-cli", "-ldflags", "-X main.appVersion=0.2", "oc-cli.go"
 
 	  bin.install "oc-cli"
 	  bash_completion.install "bash-completion/oc-cli"
